eee4501194355ba7bd22c2cbd5f7ffbf2754d467
Allal EL BAKOURI <allal.elbakourii@gmial.com>
2023-10-28 20:24:27 +0100

    duplicated servers

--- sources/classes/ConfParser.cpp
+++ sources/classes/ConfParser.cpp
@@ -21,15 +21,23 @@ void ConfParser::parse(const std::string &config_file)
 		throw std::runtime_error("Error: Server config file is not well formated 10");
 	for (size_t i = 0; i < servers.size(); i++)
 	{
-		for (size_t j = i + 1; j < servers.size(); j++)
+		for (size_t j = i + 1; j < servers.size();)
 		{
-			if (servers[i].getPort() == servers[j].getPort() && servers[i].getHost() == servers[j].getHost())
+			if (servers[i].getPort() == servers[j].getPort() && servers[i].getHost() == servers[j].getHost() && servers[i].getServerName() == servers[j].getServerName())
 			{
 				
 				throw std::runtime_error("Error: Server config file is not well formated 11");
 			}
+			else if (servers[i].getPort() == servers[j].getPort() && servers[i].getHost() == servers[j].getHost())
+			{
+				servers.erase(servers.begin() + j);
+			}
+			else
+				j++;
 		}
 	}
+	nbr_serv = servers.size();
+	std::cout << "nb serv: " << nbr_serv  << " size " << servers.size() << std::endl;
 }
 
 void ConfParser::splitServers(std::string &content)
@@ -187,9 +195,9 @@ std::vector<Server> ConfParser::getServers()
 
 int ConfParser::print()
 {
-	std::cout << "nb server: " << nbr_serv << std::endl;
+	std::cout << "nb server: " << nbr_serv  << " size " << servers.size() << std::endl;
 
-	for (size_t i = 0; i < serv_conf.size(); i++)
+	for (size_t i = 0; i < servers.size(); i++)
 	{
 		std::cout << "getRoot: " << servers[i].getRoot() << std::endl;
 		std::cout << "getPort: " << servers[i].getPort() << std::endl;
